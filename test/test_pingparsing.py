ae78f2e9b4d5186ca18fa8205fba1eb8b5f8f5ac
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-10-15 10:21:05 +0900

    Rename a variable

--- test/test_pingparsing.py
+++ test/test_pingparsing.py
@@ -25,7 +25,7 @@ rtt min/avg/max/mdev = 61.425/99.731/212.597/27.566 ms
 # ping google.com -q -c 60:
 #   - Debian 8.2 w/ iputils-ping 20121221-5+b2
 #   - Debian 5.0.10 w/ iputils-ping 20071127-1+lenny1
-PING_DEBIAN = six.b("""PING google.com (216.58.196.238) 56(84) bytes of data.
+PING_DEBIAN_SUCCESS = six.b("""PING google.com (216.58.196.238) 56(84) bytes of data.
 
 --- google.com ping statistics ---
 60 packets transmitted, 60 received, 0% packet loss, time 59153ms
@@ -71,7 +71,7 @@ class Test_PingParsing_parse:
 
     @pytest.mark.parametrize(["ping_text", "expected"], [
         [
-            PING_DEBIAN,
+            PING_DEBIAN_SUCCESS,
             {
                 "packet_transmit": 60,
                 "packet_receive": 60,
