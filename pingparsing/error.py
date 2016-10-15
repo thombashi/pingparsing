9e88530cc8ff4ea3c37251ed7c7dba3b4d616f24
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-10-15 13:39:23 +0900

    Add PingStaticticsHeaderNotFoundError class

--- pingparsing/error.py
+++ pingparsing/error.py
@@ -7,6 +7,12 @@
 from __future__ import absolute_import
 
 
+class PingStaticticsHeaderNotFoundError(Exception):
+    """
+    Raised when a ping statistics header is not found in a parsing text.
+    """
+
+
 class EmptyPingStaticticsError(Exception):
     """
     Raised when a ping statistics is empty in a parsing text.
