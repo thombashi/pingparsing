663af3ab682bce6276768722b6085f2b47d9576e
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-03-22 21:27:18 +0900

    Modify to use fixtures

--- test/test_pingparse.py
+++ test/test_pingparse.py
@@ -12,19 +12,28 @@ import pytest
 from typepy.type import RealNumber
 
 
+@pytest.fixture
+def ping_parser():
+    return PingParsing()
+
+
+@pytest.fixture
+def transmitter():
+    return PingTransmitter()
+
+
 class Test_PingParse_parse:
 
     @pytest.mark.xfail
     @pytest.mark.parametrize(["host", "waittime", "expected"], [
         ["localhost", 3, ValueError],
     ])
-    def test_normal(self, host, waittime, expected):
-        transmitter = PingTransmitter()
+    def test_normal_waittime(
+            self, transmitter, ping_parser, host, waittime, expected):
         transmitter.destination_host = host
         transmitter.waittime = waittime
         result = transmitter.ping()
 
-        ping_parser = PingParsing()
         ping_parser.parse(result.stdout)
 
         assert RealNumber(ping_parser.packet_transmit).is_type()
