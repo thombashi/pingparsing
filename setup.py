f76d777092e6dca227276d11463fbd06fdbf37dd
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-06-05 00:06:07 +0900

    Bump version to 0.8.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="pingparsing",
-    version="0.7.0",
+    version="0.8.0",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/pingparsing",
