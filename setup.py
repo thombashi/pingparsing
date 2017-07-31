f527dbf4cd34d0a47e8ceea00b8cc6650f1d8b55
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-07-31 11:45:18 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -27,14 +27,11 @@ with io.open(os.path.join(
 with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
     install_requires = [line.strip() for line in f if line.strip()]
 
-if any([
-    sys.version_info.major < 3,
-    sys.version_info.major == 3 and sys.version_info.minor < 3,
-]):
-    install_requires.append("ipaddress")
-
 with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
-    tests_require = [line.strip() for line in f if line.strip()]
+    tests_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
+    docs_requires = [line.strip() for line in f if line.strip()]
 
 setuptools.setup(
     name="pingparsing",
@@ -50,7 +47,12 @@ setuptools.setup(
     install_requires=install_requires,
     packages=setuptools.find_packages(exclude=["test*"]),
     setup_requires=pytest_runner,
-    tests_require=tests_require,
+    tests_require=tests_requires,
+    extras_require={
+        "test": tests_requires,
+        "docs": docs_requires,
+    },
+
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
