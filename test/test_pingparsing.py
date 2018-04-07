7ddd6a551840f9b6396f7691431499b957d7cf24
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-07 15:08:54 +0900

    Modify test case formatting

--- test/test_pingparsing.py
+++ test/test_pingparsing.py
@@ -6,20 +6,22 @@
 
 from __future__ import absolute_import
 
+from textwrap import dedent
+
 import pytest
 import six
+
 from pingparsing import EmptyPingStatisticsError
 
 from .common import PingTestData, ping_parser
 from .data import DEBIAN_SUCCESS, WINDOWS7SP1_SUCCESS
 
-
-PING_FEDORA_EMPTY_BODY = six.b("""
+PING_FEDORA_EMPTY_BODY = six.b("""\
 PING 192.168.0.1 (192.168.0.1) 56(84) bytes of data.
 
 --- 192.168.0.1 ping statistics ---
 """)
-PING_WINDOWS_INVALID = """
+PING_WINDOWS_INVALID = """\
 Pinging 192.168.207.100 with 32 bytes of data:
 Request timed out.
 Request timed out.
@@ -30,11 +32,12 @@ Ping statistics for 192.168.207.100:
 """
 
 DEBIAN_UNREACHABLE_0 = PingTestData(
-    """PING 192.168.207.100 (192.168.207.100) 56(84) bytes of data.
+    dedent("""\
+    PING 192.168.207.100 (192.168.207.100) 56(84) bytes of data.
 
---- 192.168.207.100 ping statistics ---
-5 packets transmitted, 0 received, 100% packet loss, time 4009ms
-""",
+    --- 192.168.207.100 ping statistics ---
+    5 packets transmitted, 0 received, 100% packet loss, time 4009ms
+    """),
     {
         "destination": "192.168.207.100",
         "packet_transmit": 5,
@@ -54,12 +57,13 @@ DEBIAN_UNREACHABLE_2 = PingTestData(
     DEBIAN_UNREACHABLE_1.value + "\n", DEBIAN_UNREACHABLE_0.expected)
 
 FEDORA_DUP_LOSS = PingTestData(
-    """PING 192.168.0.1 (192.168.0.1) 56(84) bytes of data.
+    dedent("""\
+    PING 192.168.0.1 (192.168.0.1) 56(84) bytes of data.
 
---- 192.168.0.1 ping statistics ---
-1688 packets transmitted, 1553 received, +1 duplicates, 7% packet loss, time 2987ms
-rtt min/avg/max/mdev = 0.282/0.642/11.699/0.699 ms, pipe 2, ipg/ewma 1.770/0.782 ms
-""",
+    --- 192.168.0.1 ping statistics ---
+    1688 packets transmitted, 1553 received, +1 duplicates, 7% packet loss, time 2987ms
+    rtt min/avg/max/mdev = 0.282/0.642/11.699/0.699 ms, pipe 2, ipg/ewma 1.770/0.782 ms
+    """),
     {
         "destination": "192.168.0.1",
         "packet_transmit": 1688,
@@ -74,16 +78,17 @@ rtt min/avg/max/mdev = 0.282/0.642/11.699/0.699 ms, pipe 2, ipg/ewma 1.770/0.782
         "rtt_avg": 0.642,
     })
 FEDORA_UNREACHABLE = PingTestData(
-    """PING 192.168.207.100 (192.168.207.100) 56(84) bytes of data.
-From 192.168.207.128 icmp_seq=1 Destination Host Unreachable
-From 192.168.207.128 icmp_seq=2 Destination Host Unreachable
-From 192.168.207.128 icmp_seq=3 Destination Host Unreachable
-From 192.168.207.128 icmp_seq=4 Destination Host Unreachable
-From 192.168.207.128 icmp_seq=5 Destination Host Unreachable
+    dedent("""\
+    PING 192.168.207.100 (192.168.207.100) 56(84) bytes of data.
+    From 192.168.207.128 icmp_seq=1 Destination Host Unreachable
+    From 192.168.207.128 icmp_seq=2 Destination Host Unreachable
+    From 192.168.207.128 icmp_seq=3 Destination Host Unreachable
+    From 192.168.207.128 icmp_seq=4 Destination Host Unreachable
+    From 192.168.207.128 icmp_seq=5 Destination Host Unreachable
 
---- 192.168.207.100 ping statistics ---
-5 packets transmitted, 0 received, +5 errors, 100% packet loss, time 4003ms
-""",
+    --- 192.168.207.100 ping statistics ---
+    5 packets transmitted, 0 received, +5 errors, 100% packet loss, time 4003ms
+    """),
     {
         "destination": "192.168.207.100",
         "packet_transmit": 5,
@@ -99,17 +104,18 @@ From 192.168.207.128 icmp_seq=5 Destination Host Unreachable
     })
 
 OSX_SUCCESS_0 = PingTestData(
-    """PING google.com (172.217.6.238): 56 data bytes
-64 bytes from 172.217.6.238: icmp_seq=0 ttl=53 time=20.482 ms
-64 bytes from 172.217.6.238: icmp_seq=1 ttl=53 time=32.550 ms
-64 bytes from 172.217.6.238: icmp_seq=2 ttl=53 time=32.013 ms
-64 bytes from 172.217.6.238: icmp_seq=3 ttl=53 time=28.498 ms
-64 bytes from 172.217.6.238: icmp_seq=4 ttl=53 time=46.093 ms
+    dedent("""\
+    PING google.com (172.217.6.238): 56 data bytes
+    64 bytes from 172.217.6.238: icmp_seq=0 ttl=53 time=20.482 ms
+    64 bytes from 172.217.6.238: icmp_seq=1 ttl=53 time=32.550 ms
+    64 bytes from 172.217.6.238: icmp_seq=2 ttl=53 time=32.013 ms
+    64 bytes from 172.217.6.238: icmp_seq=3 ttl=53 time=28.498 ms
+    64 bytes from 172.217.6.238: icmp_seq=4 ttl=53 time=46.093 ms
 
---- google.com ping statistics ---
-5 packets transmitted, 5 packets received, 0.0% packet loss
-round-trip min/avg/max/stddev = 20.482/31.927/46.093/8.292 ms
-""",
+    --- google.com ping statistics ---
+    5 packets transmitted, 5 packets received, 0.0% packet loss
+    round-trip min/avg/max/stddev = 20.482/31.927/46.093/8.292 ms
+    """),
     {
         "destination": "google.com",
         "packet_transmit": 5,
@@ -124,12 +130,13 @@ round-trip min/avg/max/stddev = 20.482/31.927/46.093/8.292 ms
         "rtt_mdev": 8.292,
     })
 OSX_SUCCESS_1 = PingTestData(
-    """PING github.com (192.30.255.113): 56 data bytes
+    dedent("""\
+    PING github.com (192.30.255.113): 56 data bytes
 
---- github.com ping statistics ---
-10 packets transmitted, 10 packets received, 0.0% packet loss
-round-trip min/avg/max/stddev = 218.391/283.477/405.879/70.170 ms
-""",
+    --- github.com ping statistics ---
+    10 packets transmitted, 10 packets received, 0.0% packet loss
+    round-trip min/avg/max/stddev = 218.391/283.477/405.879/70.170 ms
+    """),
     {
         "destination": "github.com",
         "packet_transmit": 10,
@@ -144,11 +151,12 @@ round-trip min/avg/max/stddev = 218.391/283.477/405.879/70.170 ms
         "rtt_mdev": 70.170,
     })
 OSX_UNREACHABLE_0 = PingTestData(
-    """PING twitter.com (59.24.3.173): 56 data bytes
-^C
---- twitter.com ping statistics ---
-59 packets transmitted, 0 packets received, 100.0% packet loss
-""",
+    dedent("""\
+    PING twitter.com (59.24.3.173): 56 data bytes
+    ^C
+    --- twitter.com ping statistics ---
+    59 packets transmitted, 0 packets received, 100.0% packet loss
+    """),
     {
         "destination": "twitter.com",
         "packet_transmit": 59,
@@ -163,11 +171,12 @@ OSX_UNREACHABLE_0 = PingTestData(
         "packet_duplicate_count": None,
     })
 OSX_UNREACHABLE_1 = PingTestData(
-    """PING twitter.com (31.13.78.66): 56 data bytes
+    dedent("""\
+    PING twitter.com (31.13.78.66): 56 data bytes
 
---- twitter.com ping statistics ---
-10 packets transmitted, 0 packets received, 100.0% packet loss
-""",
+    --- twitter.com ping statistics ---
+    10 packets transmitted, 0 packets received, 100.0% packet loss
+    """),
     {
         "destination": "twitter.com",
         "packet_transmit": 10,
@@ -183,12 +192,13 @@ OSX_UNREACHABLE_1 = PingTestData(
     })
 
 ALPINE_LINUX_SUCCESS = PingTestData(
-    """PING heise.de (193.99.144.80): 56 data bytes
+   dedent("""\
+   PING heise.de (193.99.144.80): 56 data bytes
 
---- heise.de ping statistics ---
-5 packets transmitted, 5 packets received, 0% packet loss
-round-trip min/avg/max = 0.638/0.683/0.746 ms
-""",
+    --- heise.de ping statistics ---
+    5 packets transmitted, 5 packets received, 0% packet loss
+    round-trip min/avg/max = 0.638/0.683/0.746 ms
+    """),
     {
         "destination": "heise.de",
         "packet_transmit": 5,
@@ -203,23 +213,24 @@ round-trip min/avg/max = 0.638/0.683/0.746 ms
         "rtt_mdev": None,
     })
 ALPINE_LINUX_DUP_LOSS = PingTestData(
-    """PING 192.168.2.106 (192.168.2.106): 56 data bytes
-64 bytes from 192.168.2.106: seq=0 ttl=64 time=0.936 ms
-64 bytes from 192.168.2.106: seq=0 ttl=64 time=1.003 ms (DUP!)
-64 bytes from 192.168.2.106: seq=1 ttl=64 time=0.802 ms
-64 bytes from 192.168.2.106: seq=2 ttl=64 time=0.696 ms
-64 bytes from 192.168.2.106: seq=3 ttl=64 time=0.664 ms
-64 bytes from 192.168.2.106: seq=4 ttl=64 time=1.194 ms
-64 bytes from 192.168.2.106: seq=5 ttl=64 time=0.613 ms
-64 bytes from 192.168.2.106: seq=6 ttl=64 time=0.898 ms
-64 bytes from 192.168.2.106: seq=8 ttl=64 time=1.066 ms
-64 bytes from 192.168.2.106: seq=9 ttl=64 time=1.144 ms
-64 bytes from 192.168.2.106: seq=9 ttl=64 time=1.219 ms (DUP!)
+    dedent("""\
+    PING 192.168.2.106 (192.168.2.106): 56 data bytes
+    64 bytes from 192.168.2.106: seq=0 ttl=64 time=0.936 ms
+    64 bytes from 192.168.2.106: seq=0 ttl=64 time=1.003 ms (DUP!)
+    64 bytes from 192.168.2.106: seq=1 ttl=64 time=0.802 ms
+    64 bytes from 192.168.2.106: seq=2 ttl=64 time=0.696 ms
+    64 bytes from 192.168.2.106: seq=3 ttl=64 time=0.664 ms
+    64 bytes from 192.168.2.106: seq=4 ttl=64 time=1.194 ms
+    64 bytes from 192.168.2.106: seq=5 ttl=64 time=0.613 ms
+    64 bytes from 192.168.2.106: seq=6 ttl=64 time=0.898 ms
+    64 bytes from 192.168.2.106: seq=8 ttl=64 time=1.066 ms
+    64 bytes from 192.168.2.106: seq=9 ttl=64 time=1.144 ms
+    64 bytes from 192.168.2.106: seq=9 ttl=64 time=1.219 ms (DUP!)
 
---- 192.168.2.106 ping statistics ---
-10 packets transmitted, 9 packets received, 2 duplicates, 10% packet loss
-round-trip min/avg/max = 0.613/0.930/1.219 ms
-""",
+    --- 192.168.2.106 ping statistics ---
+    10 packets transmitted, 9 packets received, 2 duplicates, 10% packet loss
+    round-trip min/avg/max = 0.613/0.930/1.219 ms
+    """),
     {
         "destination": "192.168.2.106",
         "packet_transmit": 10,
@@ -235,24 +246,24 @@ round-trip min/avg/max = 0.613/0.930/1.219 ms
     })
 
 WINDOWS10_LOSS = PingTestData(
-    """
-Pinging 192.168.2.106 with 32 bytes of data:
-Reply from 192.168.2.106: bytes=32 time=16ms TTL=64
-Reply from 192.168.2.106: bytes=32 time=6ms TTL=64
-Reply from 192.168.2.106: bytes=32 time=12ms TTL=64
-Reply from 192.168.2.106: bytes=32 time=16ms TTL=64
-Request timed out.
-Reply from 192.168.2.106: bytes=32 time=8ms TTL=64
-Reply from 192.168.2.106: bytes=32 time=33ms TTL=64
-Reply from 192.168.2.106: bytes=32 time=13ms TTL=64
-Reply from 192.168.2.106: bytes=32 time=23ms TTL=64
-Reply from 192.168.2.106: bytes=32 time<1ms TTL=64
+    dedent("""\
+    Pinging 192.168.2.106 with 32 bytes of data:
+    Reply from 192.168.2.106: bytes=32 time=16ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time=6ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time=12ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time=16ms TTL=64
+    Request timed out.
+    Reply from 192.168.2.106: bytes=32 time=8ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time=33ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time=13ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time=23ms TTL=64
+    Reply from 192.168.2.106: bytes=32 time<1ms TTL=64
 
-Ping statistics for 192.168.2.106:
-    Packets: Sent = 10, Received = 9, Lost = 1 (10% los
-Approximate round trip times in milli-seconds:
-    Minimum = 0ms, Maximum = 33ms, Average = 14ms
-""",
+    Ping statistics for 192.168.2.106:
+        Packets: Sent = 10, Received = 9, Lost = 1 (10% los
+    Approximate round trip times in milli-seconds:
+        Minimum = 0ms, Maximum = 33ms, Average = 14ms
+    """),
     {
         "destination": "192.168.2.106",
         "packet_transmit": 10,
@@ -267,16 +278,16 @@ Approximate round trip times in milli-seconds:
         "rtt_mdev": None,
     })
 WINDOWS_UNREACHABLE_0 = PingTestData(
-    """
-Pinging 192.168.207.100 with 32 bytes of data:
-Request timed out.
-Request timed out.
-Request timed out.
-Request timed out.
+    dedent("""\
+    Pinging 192.168.207.100 with 32 bytes of data:
+    Request timed out.
+    Request timed out.
+    Request timed out.
+    Request timed out.
 
-Ping statistics for 192.168.207.100:
-    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
-""",
+    Ping statistics for 192.168.207.100:
+        Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+    """),
     {
         "destination": "192.168.207.100",
         "packet_transmit": 4,
@@ -325,13 +336,13 @@ class Test_PingParsing_parse(object):
         assert ping_parser.as_dict() == test_data.expected
 
     def test_empty(self, ping_parser):
-        ping_parser.parse("""
-PING google.com (216.58.196.238) 56(84) bytes of data.
+        ping_parser.parse(dedent("""\
+            PING google.com (216.58.196.238) 56(84) bytes of data.
 
---- google.com ping statistics ---
-60 packets transmitted, 60 received, 0% packet loss, time 59153ms
-rtt min/avg/max/mdev = 61.425/99.731/212.597/27.566 ms
-""")
+            --- google.com ping statistics ---
+            60 packets transmitted, 60 received, 0% packet loss, time 59153ms
+            rtt min/avg/max/mdev = 61.425/99.731/212.597/27.566 ms
+            """))
         ping_parser.parse("")
 
         assert ping_parser.destination is None
