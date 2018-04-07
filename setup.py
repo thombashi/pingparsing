38051230886363300d7f64754dca7663ce439460
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-07 14:21:28 +0900

    Add release command

--- setup.py
+++ setup.py
@@ -17,6 +17,24 @@ REQUIREMENT_DIR = "requirements"
 pkg_info = {}
 
 
+class ReleaseCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        tag = "v{}".format(pkg_info["__version__"])
+
+        print("Pushing git tags: {}".format(tag))
+
+        os.system("git tag {}".format(tag))
+        os.system("git push --tags")
+
+
 with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
     exec(f.read(), pkg_info)
 
@@ -79,6 +97,9 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Networking",
     ],
+    cmdclass={
+        "release": ReleaseCommand,
+    },
     entry_points={
         "console_scripts": [
             "pingparsing=pingparsing.cli:main",
