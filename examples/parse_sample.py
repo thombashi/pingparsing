2b61fefe6f3a56b4bcadcecea423ced0b96c282c
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-29 11:00:29 +0900

    Update an example

--- examples/parse_sample.py
+++ examples/parse_sample.py
@@ -17,16 +17,16 @@ import pingparsing
 def main():
     parser = pingparsing.PingParsing()
     stats = parser.parse(dedent("""\
-        PING twitter.com (104.244.42.65) 56(84) bytes of data.
-        64 bytes from 104.244.42.65: icmp_seq=1 ttl=53 time=68.5 ms
-        64 bytes from 104.244.42.65: icmp_seq=2 ttl=53 time=67.7 ms
-        64 bytes from 104.244.42.65: icmp_seq=3 ttl=53 time=65.6 ms
-        64 bytes from 104.244.42.65: icmp_seq=4 ttl=53 time=65.6 ms
-        64 bytes from 104.244.42.65: icmp_seq=5 ttl=53 time=64.9 ms
+        PING google.com (74.125.24.100) 56(84) bytes of data.
+        [1524930937.003555] 64 bytes from 74.125.24.100: icmp_seq=1 ttl=39 time=148 ms
+        [1524930937.787175] 64 bytes from 74.125.24.100: icmp_seq=2 ttl=39 time=137 ms
+        [1524930938.787642] 64 bytes from 74.125.24.100: icmp_seq=3 ttl=39 time=137 ms
+        [1524930939.787653] 64 bytes from 74.125.24.100: icmp_seq=4 ttl=39 time=136 ms
+        [1524930940.788365] 64 bytes from 74.125.24.100: icmp_seq=5 ttl=39 time=136 ms
 
-        --- twitter.com ping statistics ---
-        5 packets transmitted, 5 received, 0% packet loss, time 4003ms
-        rtt min/avg/max/mdev = 64.912/66.494/68.524/1.430 ms
+        --- google.com ping statistics ---
+        5 packets transmitted, 5 received, 0% packet loss, time 4001ms
+        rtt min/avg/max/mdev = 136.537/139.174/148.006/4.425 ms
         """))
 
     print("[ping statistics]")
