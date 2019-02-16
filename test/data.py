e2651c966d2ac39a950aaaa3f2a16ffa9f1b0a40
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-02-16 14:26:00 +0900

    Modify indentation

--- test/data.py
+++ test/data.py
@@ -19,12 +19,12 @@ from .common import PingTestData
 PING_DEBIAN_SUCCESS_0 = six.b(
     dedent(
         """\
-    PING google.com (216.58.196.238) 56(84) bytes of data.
+        PING google.com (216.58.196.238) 56(84) bytes of data.
 
-    --- google.com ping statistics ---
-    60 packets transmitted, 60 received, 0% packet loss, time 59153ms
-    rtt min/avg/max/mdev = 61.425/99.731/212.597/27.566 ms
-    """
+        --- google.com ping statistics ---
+        60 packets transmitted, 60 received, 0% packet loss, time 59153ms
+        rtt min/avg/max/mdev = 61.425/99.731/212.597/27.566 ms
+        """
     )
 )
 
@@ -33,17 +33,17 @@ PING_DEBIAN_SUCCESS_0 = six.b(
 PING_UBUNTU_SUCCESS_0 = six.b(
     dedent(
         """\
-    PING twitter.com (104.244.42.65) 56(84) bytes of data.
-    64 bytes from 104.244.42.65: icmp_seq=1 ttl=53 time=68.5 ms
-    64 bytes from 104.244.42.65: icmp_seq=2 ttl=53 time=67.7 ms
-    64 bytes from 104.244.42.65: icmp_seq=3 ttl=53 time=65.6 ms
-    64 bytes from 104.244.42.65: icmp_seq=4 ttl=53 time=65.6 ms
-    64 bytes from 104.244.42.65: icmp_seq=5 ttl=53 time=64.9 ms
+        PING twitter.com (104.244.42.65) 56(84) bytes of data.
+        64 bytes from 104.244.42.65: icmp_seq=1 ttl=53 time=68.5 ms
+        64 bytes from 104.244.42.65: icmp_seq=2 ttl=53 time=67.7 ms
+        64 bytes from 104.244.42.65: icmp_seq=3 ttl=53 time=65.6 ms
+        64 bytes from 104.244.42.65: icmp_seq=4 ttl=53 time=65.6 ms
+        64 bytes from 104.244.42.65: icmp_seq=5 ttl=53 time=64.9 ms
 
-    --- twitter.com ping statistics ---
-    5 packets transmitted, 5 received, 0% packet loss, time 4003ms
-    rtt min/avg/max/mdev = 64.912/66.494/68.524/1.430 ms
-    """
+        --- twitter.com ping statistics ---
+        5 packets transmitted, 5 received, 0% packet loss, time 4003ms
+        rtt min/avg/max/mdev = 64.912/66.494/68.524/1.430 ms
+        """
     )
 )
 
