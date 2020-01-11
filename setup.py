698f443d47416d9b6ebf573b8100efca57f15734
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-11 23:26:15 +0900

    Add long_description_content_type

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setuptools.setup(
     keywords=["cli-app", "network", "ping", "parser", "transmitter"],
     license=pkg_info["__license__"],
     long_description=long_description,
+    long_description_content_type="text/x-rst",
     packages=setuptools.find_packages(exclude=["test*"]),
     project_urls={
         "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
