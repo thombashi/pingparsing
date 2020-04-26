d9d2bd00a3d5d5cce5c9b07112d6d74382e4edbe
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-04-26 10:21:30 +0900

    Replace literal with a constant variable

--- pingparsing/__main__.py
+++ pingparsing/__main__.py
@@ -188,7 +188,7 @@ def initialize_logger(log_level: str) -> None:
         set_logger(is_enable=False)
         return
 
-    if log_level == "DEBUG":
+    if log_level == LogLevel.DEBUG:
         log_format = (
             "<level>{level: <8}</level> | "
             "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
