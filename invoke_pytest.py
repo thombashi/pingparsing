5a09b88399b34ea8a5185fe1bcdff5f3f7ac7619
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-05-10 00:34:44 +0900

    Add PYTEST_MD_REPORT_COLOR environment variable setting

--- invoke_pytest.py
+++ invoke_pytest.py
@@ -6,10 +6,12 @@ because of multiprocessing:
 https://py.rtfd.io/en/latest/faq.html?highlight=cmdline#issues-with-py-test-multiprocess-and-setuptools
 """
 
+import os
 import sys
 
 import py
 
 
 if __name__ == "__main__":
+    os.environ["PYTEST_MD_REPORT_COLOR"] = "text"
     sys.exit(py.test.cmdline.main())
