2c3a2f91999596e0e5e357e9124d122127f65758
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-05-05 10:59:22 +0900

    Fix a lint error

--- pingparsing/__main__.py
+++ pingparsing/__main__.py
@@ -205,7 +205,8 @@ def initialize_logger(log_level: str) -> None:
     if log_level == LogLevel.DEBUG:
         log_format = (
             "<level>{level: <8}</level> | "
-            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+            "<level>{message}</level>"
         )
 
         try:
