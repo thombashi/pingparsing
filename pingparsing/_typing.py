38691762658fb5ecfe2cc1603f5be90ff3ebe50e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-05-05 15:54:19 +0900

    Modify a type alias

--- pingparsing/_typing.py
+++ pingparsing/_typing.py
@@ -6,4 +6,4 @@ import humanreadable as hr
 
 TimeArg = Union[hr.Time, int, str, None]
 IcmpReplies = Sequence[Dict[str, Union[str, bool, float, int, datetime]]]
-PingAddOpts = Union[str, Sequence]
+PingAddOpts = Union[str, Sequence[str]]
