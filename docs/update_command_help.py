574771130ed420416fe4efed0c35138c3181415a
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-02-27 15:27:14 +0900

    Add error handling

--- docs/update_command_help.py
+++ docs/update_command_help.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 from textwrap import indent
 
-from subprocrunner import SubprocessRunner
+from subprocrunner import CalledProcessError, SubprocessRunner
 
 
 def main():
     proc = SubprocessRunner(["pingparsing", "-h"])
-    proc.run(env=dict(os.environ, LC_ALL="C.UTF-8"))
+    try:
+        proc.run(env=dict(os.environ, LC_ALL="C.UTF-8"), check=True)
+    except CalledProcessError as e:
+        print(f"[ERROR] {e}\n{e.stderr}", file=sys.stderr)
+        sys.exit(1)
+
     help_file_path = "pages/usage/cli_help.txt"
 
     print(help_file_path)
